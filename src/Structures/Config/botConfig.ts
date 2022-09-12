c8942e3333e003f8bea9cb61f28839757993dbd7
Itsbueno <82325368+buenindvl@users.noreply.github.com>
2022-09-12 10:10:23 -0300

    Ops

--- src/Structures/Config/botConfig.ts
+++ src/Structures/Config/botConfig.ts
@@ -1,4 +1,4 @@
 export const botConfig = {
-  token: "MTAxODU5NDkzOTQ1MTQ4NjIyOA.GKGrMj.OudCEBk69DVAhmIk3_xNB9R3fOdfaO9Z1YLykk", // Bot Token
+  token: "", // Bot Token
   prefix: "!", // Bot prefix
 }
